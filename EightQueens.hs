21df7681101934e28b71c0839d41ab837c771f70
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-02-16 21:54:45 -0800

    Main runs tests

---
+++ EightQueens.hs
@@ -0,0 +1,12 @@
+import Test.HUnit
+
+-- Tests
+tests = TestList $ map TestCase
+    [assertEqual "add tests here" 1 1
+    ]
+
+runTests = do
+    print "Eight Queens Puzzle"
+    runTestTT tests
+
+main = runTests
