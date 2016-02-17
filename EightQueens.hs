f110dc771374599dc42b5952c0319665758c86db
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-02-16 22:22:17 -0800

    Stub functions needed to calculate solutions

--- EightQueens.hs
+++ EightQueens.hs
@@ -3,6 +3,21 @@ module EightQueens where
 import Test.HUnit
 
 
+solve :: [Int] -> [Int]
+solve xs = xs
+
+okToAddLevel :: [Int] -> Int -> Bool
+okToAddLevel = undefined
+
+okToAddUp :: [Int] -> Int -> Bool
+okToAddUp = undefined
+
+okToAddDown :: [Int] -> Int -> Bool
+okToAddDown = undefined
+
+runEightQueens = do
+    solve []
+
 -- Tests
 tests = TestList $ map TestCase
     [assertEqual "add tests here" 1 1
